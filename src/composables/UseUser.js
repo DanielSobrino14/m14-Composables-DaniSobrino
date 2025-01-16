e11315cbfef7753d620e24afb223900cb9b08a1e
DanielSobrino14 <daniel.sobrino.7e7@itb.cat>
2025-01-16 17:23:41 +0100

    ejercicio totalmente finalizado

--- src/composables/UseUser.js
+++ src/composables/UseUser.js
@@ -6,12 +6,8 @@ export default function useUser() {
   const user = ref(null);
 
   const llegirUsuari = async (id) => {
-    try {
       const response = await fetch(`${baseURL}/${id}`);
       user.value = await response.json();
-    } catch (error) {
-      console.error(`Error obtenint l'usuari amb ID ${id}:`, error);
-    }
   };
 
   return {
